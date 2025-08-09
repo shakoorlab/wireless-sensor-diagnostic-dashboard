4a4116e49d4be3dfcd59a26c5d2a1b8da901d22e
JaccobStanton <jstanton23@my.stlcc.edu>
2025-08-08 19:39:59 -0500

    Wrapped async controller in routes

--- server/src/routes/selectedFieldMetrics.js
+++ server/src/routes/selectedFieldMetrics.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { getHourlyStatus } from '../controllers/selectedFieldMetrics.js';
+import asyncHandler from '../middlewares/asyncHandler.js';
 
 const router = express.Router();
 
 // router.get('/reporting-summary', getReportingSummary); // for pie chart
 // router.get('/inactive-sensors', getInactiveSensors); // for sensor activity list
-router.get('/hourly-status', getHourlyStatus); //for hourly status table
+router.get('/hourly-status', asyncHandler(getHourlyStatus)); //for hourly status table
 
 export default router;
