9f9f927afa6691be4a76e5a4b578a2449906a60f
JaccobStanton <jstanton23@my.stlcc.edu>
2025-08-18 18:47:01 -0500

    Now supports getHourlyStatus and getWeeklyStatus

--- server/src/routes/selectedFieldMetrics.js
+++ server/src/routes/selectedFieldMetrics.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { getHourlyStatus } from '../controllers/selectedFieldMetrics.js';
+import { getWeeklyStatus } from '../controllers/selectedFieldMetrics.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
 
 const router = express.Router();
 
 // router.get('/reporting-summary', getReportingSummary); // for pie chart
 // router.get('/inactive-sensors', getInactiveSensors); // for sensor activity list
-router.get('/hourly-status', asyncHandler(getHourlyStatus)); //for hourly status table
+router.get('/hourly-status', asyncHandler(getHourlyStatus)); //for daily status table
+router.get('/weekly-status', getWeeklyStatus); // for weekly status table
 
 export default router;
